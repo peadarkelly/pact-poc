f19152db4fe0fd44b746862c5d1a036cb2f956ba
Peadar Kelly <peadark@kainos.com>
2020-05-07 15:01:04 +0100

    Consumer API now calls Provider API

--- pact-consumer-api/src/main/java/com/example/pactconsumerapi/PactConsumerApiApplication.java
+++ pact-consumer-api/src/main/java/com/example/pactconsumerapi/PactConsumerApiApplication.java
@@ -2,12 +2,20 @@ package com.example.pactconsumerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PactConsumerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PactConsumerApiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
--- pact-consumer-api/src/main/java/com/example/pactconsumerapi/services/UserEligibilityService.java
+++ pact-consumer-api/src/main/java/com/example/pactconsumerapi/services/UserEligibilityService.java
@@ -1,13 +1,35 @@
 package com.example.pactconsumerapi.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+class EligibilityResponse {
+
+    private boolean isEligible;
+
+    public EligibilityResponse() {}
+
+    public boolean isEligible() {
+        return this.isEligible;
+    }
+
+    public void setEligible(boolean eligible) {
+        this.isEligible = eligible;
+    }
+}
 
 @Service
 public class UserEligibilityService {
 
     public boolean isUserEligible(String email) {
-        // TODO - Call Provider API
-        return true;
+        RestTemplate restTemplate = new RestTemplate();
+
+        EligibilityResponse response = restTemplate.getForObject(
+                String.format("http://localhost:8081/is-eligible?email=%s", email),
+                EligibilityResponse.class
+        );
+
+        return response.isEligible();
     }
 
 }
